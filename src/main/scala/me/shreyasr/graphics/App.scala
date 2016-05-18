6cca685463ab93367b5b9f321a2f07a63a82169d
Shreyas <skraman1999@gmail.com>
2016-05-18 16:54:36 -0700

    Add canvas

--- src/main/scala/me/shreyasr/graphics/App.scala
+++ src/main/scala/me/shreyasr/graphics/App.scala
@@ -1,8 +1,11 @@
 package me.shreyasr.graphics
 
+import org.scalajs.dom.window
+import org.scalajs.dom.CanvasRenderingContext2D
+import org.scalajs.dom.html.Canvas
 import org.scalajs.jquery.jQuery
 
-import scala.scalajs.js.JSApp
+import scala.scalajs.js.{JSApp, timers}
 
 object App extends JSApp {
 
@@ -11,11 +14,26 @@ object App extends JSApp {
   }
 
   def setupUi(): Unit = {
-    jQuery("#click-me-button").click(onButtonClick _)
-    jQuery("body").prepend("<p>Hello World</p>")
+    val canvas = jQuery("canvas").get(0).asInstanceOf[Canvas]
+    canvas.style.background = "#0000FF"
+    val ctx = canvas.getContext("2d").asInstanceOf[CanvasRenderingContext2D]
+    timers.setInterval(100)(() -> update(ctx))
   }
 
-  def onButtonClick(): Unit = {
-    jQuery("body").append("<p>You clicked the button!</p>")
+  def update(g: CanvasRenderingContext2D): Unit = {
+    g.canvas.width = window.innerWidth
+    g.canvas.height = window.innerHeight
+
+    g.fillStyle="#FF0000"
+    g.strokeStyle="#00FF00"
+    g.lineWidth=50
+    g.fill()
+    g.beginPath()
+    g.moveTo(0, 0)
+    g.lineTo(0, 400)
+    g.lineTo(400, 400)
+    g.lineTo(400, 0)
+    g.lineTo(0, 0)
+    g.stroke()
   }
 }
